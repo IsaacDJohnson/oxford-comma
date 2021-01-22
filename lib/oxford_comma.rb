13d3bbcc86b3901896a572835e6a8fe8d4491bb0
Isaacjohnson <isaac.designws@gmail.com>
2021-01-22 13:40:16 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+def oxford_comma(fruit)
+    
+    if fruit.length == 1
+        fruit.join
+    elsif fruit.length == 2
+        fruit.join(" and ")
+    else
+        final = fruit.pop
+        fruit.join(", ") << ", and #{final}"
+    end
 
 end
